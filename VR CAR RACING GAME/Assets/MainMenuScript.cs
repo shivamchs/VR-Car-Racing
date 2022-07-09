a5be922d52a31f6abbb406adbc8714f8e2ce47cd
Adarsh <adarsh07singh2003@gmail.com>
2022-07-09 21:09:35 +0530

    UI Main Menu done

---
+++ VR CAR RACING GAME/Assets/MainMenuScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+   public void playTheGame(){
+    SceneManager.LoadScene("Game");
+   } 
+   public void quitTheGame(){
+    Debug.Log("QUIT THE GAME");
+    Application.Quit();
+   }
+}
